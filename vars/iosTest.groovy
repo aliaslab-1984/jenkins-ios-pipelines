d6bb8e807969783041170c3b6a7a5cf1a34edd26
dmazzoni <davidemazzoni@yahoo.it>
2019-02-22 12:33:52 +0100

    Add branch support

--- vars/iosTest.groovy
+++ vars/iosTest.groovy
@@ -4,13 +4,14 @@ def call(Map config) {
     def credentialsId = config.credentialsId
     def gitUri = config.gitUri
     def coverageFilePath = config.coverageFilePath
+    def gitBranch = config.gitBranch ?: 'master'
 
     if (gitUri == null || credentialsId == null || coverageFilePath == null) {
         throw new IllegalStateException('Missing configuration arguments')
     }
 
     stage ('Checkout') {
-        git credentialsId: credentialsId, url: gitUri
+        git credentialsId: credentialsId, url: gitUri, branch: gitBranch
     }
 
     stage ('Fetch Dependencies') {
