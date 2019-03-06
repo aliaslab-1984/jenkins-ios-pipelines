fd18ebc05c1c812ee86d65fa70cd9b7de0c88d2d
dmazzoni <davidemazzoni@yahoo.it>
2019-03-06 17:45:52 +0100

    Add ‘Lint’ stage

--- vars/iosTest.groovy
+++ vars/iosTest.groovy
@@ -5,6 +5,7 @@ def call(Map config) {
     def gitUri = config.gitUri
     def coverageFilePath = config.coverageFilePath
     def gitBranch = config.gitBranch ?: 'master'
+    def lintReportPattern = config.lintReportPattern ?: '**/swiftlint-results.xml'
 
     if (gitUri == null || credentialsId == null || coverageFilePath == null) {
         throw new IllegalStateException('Missing configuration arguments')
@@ -16,6 +17,11 @@ def call(Map config) {
 
     stage ('Fetch Dependencies') {
         sh "bundle exec fastlane bootstrap"
+    }
+
+    stage('Lint') {
+        sh "bundle exec fastlane sdk_lint"
+        recordIssues tool: swiftLint(pattern: lintReportPattern)
     }
 
     stage ('Unit Test') {
