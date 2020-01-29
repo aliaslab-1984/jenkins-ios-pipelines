bf8b2eeb3e8d1853ce373cb65a7183074945074d
Enrico Bonaldo <asclepix@yahoo.it>
2020-01-29 15:34:43 +0100

    Messaggi multiline

--- vars/safeCall.groovy
+++ vars/safeCall.groovy
@@ -1,10 +1,27 @@
 def call(Closure closure) {
    try {
-     slackSend color: '#ffff00', message: "${env.JOB_NAME} build ${env.BUILD_NUMBER} started";
+     slackSend color: '#ffff00', message: """
+     ${env.JOB_NAME}
+     Build: #${env.BUILD_NUMBER}
+     Status: Started
+     See: ${BUILD_URL}
+     """;
+
      closure();
-     slackSend color: 'good', message: "${env.JOB_NAME} build ${env.BUILD_NUMBER} succeded";
+
+     slackSend color: 'good', message: """
+     ${env.JOB_NAME}
+     Build: #${env.BUILD_NUMBER}
+     Status: Succeded
+     See: ${BUILD_URL}
+     """;
    }
    catch (Exception | AssertionError exc) {
-     slackSend color: '#ff0000', message: "${env.JOB_NAME} build ${env.BUILD_NUMBER} failed";
+     slackSend color: '#ff0000', message: """
+     ${env.JOB_NAME}
+     Build: #${env.BUILD_NUMBER}
+     Status: Failed
+     See: ${BUILD_URL}
+     """;
    }
 }
