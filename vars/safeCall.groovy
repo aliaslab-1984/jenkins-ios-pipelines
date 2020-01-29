3fa99880da8423c0efbdd809ddf8a89bbe285183
Enrico Bonaldo <asclepix@yahoo.it>
2020-01-29 18:32:29 +0100

    Test formattato in ogni messaggio a Slack

--- vars/safeCall.groovy
+++ vars/safeCall.groovy
@@ -12,16 +12,16 @@ def call(Closure closure) {
      slackSend color: 'good', message: """
      ${env.JOB_NAME}
      Build: #${env.BUILD_NUMBER}
-     Status: Succeded
-     See: ${BUILD_URL}
+     Status: *Succeded*
+     See: <${BUILD_URL}|here>
      """;
    }
    catch (Exception | AssertionError exc) {
      slackSend color: '#ff0000', message: """
      ${env.JOB_NAME}
      Build: #${env.BUILD_NUMBER}
-     Status: Failed
-     See: ${BUILD_URL}
+     Status: *Failed*
+     See: <${BUILD_URL}|here>
      """;
    }
 }
