36bdadb63bac4b90ed1254a2ed9b4d8af26e5260
Enrico Bonaldo <asclepix@yahoo.it>
2020-01-29 12:36:58 +0100

    Introduzione di safeCall
    Temporaneo: andrà in un gruppo di pipelines a sé stante

---
+++ vars/safeCall.groovy
@@ -0,0 +1,10 @@
+def safeCall(Closure closure) {
+   try {
+     slackSend color: '#ffff00', message: "${env.JOB_NAME} started";
+     closure();
+     slackSend color: 'good', message: "${env.JOB_NAME} succeded";
+   }
+   catch (Exception | AssertionError exc) {
+     slackSend color: '#ff0000', message: "${env.JOB_NAME} failed";
+   }
+}
