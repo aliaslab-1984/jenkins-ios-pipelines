748c0bbc77bd838eabb5f5bfb76b86133798c992
Enrico Bonaldo <asclepix@yahoo.it>
2020-10-13 17:34:02 +0200

    added objcAllTests

---
+++ vars/objcAllTests.groovy
@@ -0,0 +1,33 @@
+def call(Map config) {
+
+  pipeline {
+    def credentialsId = config.credentialsId
+    def gitUri = config.gitUri
+    def gitBranch = config.gitBranch ?: 'master'
+    def lintReportPattern = config.lintReportPattern ?: '**/swiftlint-results.xml'
+    def coverageFilePath = config.coverageFilePath ?: 'fastlane/cobertura_report.xml'
+
+    if (gitUri == null || credentialsId == null) {
+        throw new IllegalStateException('Missing configuration arguments')
+    }
+
+    stage ('Checkout') {
+        git credentialsId: credentialsId, url: gitUri, branch: gitBranch
+    }
+
+    ansiColor {
+      stage ('Fetch Dependencies') {
+          sh "bundle exec fastlane sdk_bootstrap"
+      }
+      
+      stage ('All Tests') {
+          sh "bundle exec fastlane sdk_all_test"
+      }
+      
+      stage ('Coverage') {
+          cobertura coberturaReportFile: coverageFilePath
+      }
+        
+    }
+  }
+}
