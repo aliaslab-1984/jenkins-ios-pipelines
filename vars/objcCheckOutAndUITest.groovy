a76713e9bb1dd852d2163b03404d6f48848d52f2
Enrico Bonaldo <asclepix@yahoo.it>
2020-10-13 17:28:08 +0200

    Added objcCheckOutAndUITest

---
+++ vars/objcCheckOutAndUITest.groovy
@@ -0,0 +1,33 @@
+def call(Map config) {
+
+  pipeline {
+    def credentialsId = config.credentialsId
+    def gitUri = config.gitUri
+    def gitBranch = config.gitBranch ?: 'master'
+    def lintReportPattern = config.lintReportPattern ?: '**/swiftlint-results.xml'
+    def coverageFilePath = config.coverageFilePath ?: 'fastlane/cobertura_report.xml'
+
+    if (gitUri == null || credentialsId == null) {
+        throw new IllegalStateException('Missing configuration arguments')
+    }
+
+    stage ('Checkout') {
+        git credentialsId: credentialsId, url: gitUri, branch: gitBranch
+    }
+
+    ansiColor {
+      stage ('Fetch Dependencies') {
+          sh "bundle exec fastlane sdk_bootstrap"
+      }
+      
+      stage ('Unit Test') {
+          sh "bundle exec fastlane sdk_ui_test"
+      }
+      
+      stage ('Coverage') {
+          cobertura coberturaReportFile: coverageFilePath
+      }
+        
+    }
+  }
+}
