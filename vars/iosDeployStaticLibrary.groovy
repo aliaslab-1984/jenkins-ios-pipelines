860505f6b9bd68aa37377701689ab546e62bebd4
Enrico Bonaldo <asclepix@yahoo.it>
2021-01-12 15:42:19 +0100

    Build and deploy device or simul static library

--- vars/iosDeployStaticLibrary.groovy
+++ vars/iosDeployStaticLibrary.groovy
@@ -4,6 +4,7 @@ def call(Map config) {
     def credentialsId = config.credentialsId
     def gitUri = config.gitUri
     def gitBranch = config.gitBranch ?: 'master'
+    def type = config.type ?: 'universal'
 
     if (gitUri == null || credentialsId == null) {
         throw new IllegalStateException('Missing configuration arguments')
@@ -19,11 +20,23 @@ def call(Map config) {
       }
 
       stage ('Build static library') {
-          sh "bundle exec fastlane build_static"
+        if (type == "device") {
+            sh "bundle exec fastlane build_device_static"
+        } else if (type == "universal") {
+            sh "bundle exec fastlane build_universal_static"
+        } else if (type == "simulator") {
+            sh "bundle exec fastlane build_simulator_static"
+        }
       }
 
       stage ('Deploy') {
-        sh "bundle exec fastlane deploy_static"
+        if (type == "device") {
+            sh "bundle exec fastlane deploy_device_static"
+        } else if (type == "universal") {
+            sh "bundle exec fastlane deploy_universal_static"
+        } else if (type == "simulator") {
+            sh "bundle exec fastlane deploy_simulator_static"
+        }
       }
     }
   }
