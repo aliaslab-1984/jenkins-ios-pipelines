d582445aeaf7986d55b70bd3bda128f00ec72afc
Francesco Bianco <53116975+icesco@users.noreply.github.com>
2021-02-15 09:38:16 +0100

    Create iOSSonar.groovy

---
+++ vars/iOSSonar.groovy
@@ -0,0 +1,27 @@
+def call(Map config) {
+
+  pipeline {
+    def coverageFilePath = config.coverageFilePath ?: 'fastlane/cobertura_report.xml'
+    
+    sh "echo before the checkout and lint"
+
+    iosCheckOutAndLint(config)
+    
+    sh "echo after the checkout and lint"
+
+    ansiColor {
+      
+      sh "echo before the unit test"
+      
+      stage ('Unit Test') {
+          sh "bundle exec fastlane sdk_test"
+      }
+      
+      sh "echo after the unit test"
+      
+      stage ('Coverage') {
+          cobertura coberturaReportFile: coverageFilePath
+      }
+    }
+  }
+}
