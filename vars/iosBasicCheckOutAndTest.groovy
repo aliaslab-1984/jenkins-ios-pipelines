7c47d74f6073e3c68fc75dc6c2b5467a01c863ab
Francesco Bianco <53116975+icesco@users.noreply.github.com>
2021-02-15 15:11:19 +0100

    Update iosBasicCheckOutAndTest.groovy

--- vars/iosBasicCheckOutAndTest.groovy
+++ vars/iosBasicCheckOutAndTest.groovy
@@ -31,7 +31,7 @@ def call(Map config) {
       stage ('Sonar') {
          def scannerHome = tool 'Sonar iOS Docker'
          withSonarQubeEnv("Trigger sonar iOS Docker") {
-           sh "${scannerHome}/bin/sonar-scanner"
+           sh "./SmartOTPSDK/run-sonar-swift.sh"
          }
       }
         
