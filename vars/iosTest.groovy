903661ca0d1f91019d52b76fcaaa68cce45214e2
Enrico Bonaldo <asclepix@yahoo.it>
2021-05-21 16:09:58 +0200

    simplified iosTest

--- vars/iosTest.groovy
+++ vars/iosTest.groovy
@@ -3,21 +3,21 @@ def call(Map config) {
   pipeline {
     def coverageFilePath = config.coverageFilePath ?: 'fastlane/cobertura_report.xml'
     
-    sh "echo before the checkout and lint"
+    echo 'before the checkout and lint'
 
     iosCheckOutAndLint(config)
     
-    sh "echo after the checkout and lint"
+    echo 'after the checkout and lint'
 
     ansiColor {
       
-      sh "echo before the unit test"
+      echo 'before the unit test'
       
       stage ('Unit Test') {
           sh "bundle exec fastlane sdk_test"
       }
       
-      sh "echo after the unit test"
+      echo 'after the unit test'
       
       stage ('Coverage') {
           cobertura coberturaReportFile: coverageFilePath
